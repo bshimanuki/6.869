7a3e874adf40aedd149daadf1261be8fe208e877
Ethan Kim <ellagasta@gmail.com>
2016-12-02 17:24:30 -0500

    revert constants

--- constants.py
+++ constants.py
@@ -18,13 +18,13 @@ Parameters for each run.
     :param FLAG_RANDOM_FLIP:
 """
 IMAGES_PER_CAT = None
-BATCH_SIZE = 10
+BATCH_SIZE = 100
 NUM_EPOCHS = 100
 SEED = 1234
 KEEP_PROB = 0.5
-EVAL_FREQUENCY = 1 # 10 will keep the epochs the same
+EVAL_FREQUENCY = 10 # 10 will keep the epochs the same
 MIN_EVAL_STEP = EVAL_FREQUENCY # to skip unstable part at beginning
-USE_GPU = False
+USE_GPU = True
 
 # TODO: Maybe don't use these constants directly but pass in as parameters?
 FLAG_RESIZE_AND_CROP = False
