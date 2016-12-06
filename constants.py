1d5ef4474fc3a778b5f88e90c10bedeedffbe9a8
Brian Shimanuki <bshimanuki@mit.edu>
2016-12-06 05:47:33 +0000

    less reg

--- constants.py
+++ constants.py
@@ -26,8 +26,8 @@ EVAL_FREQUENCY = 10 # 10 will keep the epochs the same
 EVAL_IMAGE_FREQUENCY = 100
 MIN_EVAL_STEP = EVAL_FREQUENCY # to skip unstable part at beginning
 USE_GPU = True
-CONV_REG = 1e-5
-FF_REG = 1e-3
+CONV_REG = 1e-6
+FF_REG = 1e-4
 
 # TODO: Maybe don't use these constants directly but pass in as parameters?
 FLAG_RESIZE_AND_CROP = True
