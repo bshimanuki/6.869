2deef1c9653f6b28de35b54059f061d4f2f24ab5
Ethan Kim <ellagasta@gmail.com>
2016-12-06 03:31:34 -0500

    testing average prediction 4

--- run_test.py
+++ run_test.py
@@ -51,7 +51,7 @@ def run_test(checkpoint_file, model_name):
 
         predictions = []
         n = 0
-        for step in range(test_size // BATCH_SIZE):
+        for step in range(test_size // BATCH_SIZE/10):
             _data = sess.run(batch_data)
             single_prediction = []
             for i in range(8):
@@ -60,8 +60,8 @@ def run_test(checkpoint_file, model_name):
                 single_prediction.extend(test_predictions)
 
             average_prediction = np.average(np.array(single_prediction), axis=0)
-
-            predictions.extend(np.asarray(average_prediction))
+            print(np.shape(average_prediction))
+            predictions.extend(average_prediction.tolist())
             print('Processing batch number: %d' % n)
             n+=1
 
