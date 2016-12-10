f58089f6332c56d67bedff8ff19da0b8e3ec2f55
Ubuntu <ubuntu@ip-172-31-54-56.ec2.internal>
2016-12-10 09:39:52 +0000

    validation stuff

--- run_test.py
+++ run_test.py
@@ -14,7 +14,7 @@ def run_test(checkpoint_file, model_name):
     # Get test data
     test_data, test_labels, _ = get_inputs_crop_flip('test')
     print("Retrieved test data successfully")
-    test_size = get_size('test')
+    sample_size = get_size('test')
 
     # Initialize model
     keep_prob = 1.
@@ -50,8 +50,8 @@ def run_test(checkpoint_file, model_name):
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
         predictions = []
-        n = 0
-        for step in range(test_size // BATCH_SIZE):
+
+        for step in range(sample_size // BATCH_SIZE):
             _data = sess.run(batch_data)
 
             single_prediction = []
@@ -78,8 +78,7 @@ def run_test(checkpoint_file, model_name):
 
             predictions.extend(single_prediction.tolist()) 
             # NUM_IMAGES * 8 * NUM_CAT
-            print('Processing batch number: %d' % n)
-            n+=1
+            print('Processing batch number: %d / %d' % (step, sample_size//BATCH_SIZE))
 
         # Save predictions in pickle file
         filename = checkpoint_file.split('/')
@@ -97,14 +96,106 @@ def run_test(checkpoint_file, model_name):
 
     return prediction_file
 
+def run_validation(checkpoint_file, model_name):
+    test_data, test_labels, _ = get_inputs_crop_flip('val')
+    print("Retrieved validation data successfully")
+    sample_size = get_size('val')
+
+    keep_prob = 1.
+    if model_name == 'AlexNet':
+        model = AlexNetSmall(keep_prob)
+    elif model_name == 'VGGNet':
+        model = VGGNet(keep_prob)
+    else:
+        raise Exception('Not a valid model name')
+
+    x = tf.placeholder(TYPE, shape=(BATCH_SIZE, IMAGE_FINAL_SIZE, IMAGE_FINAL_SIZE, NUM_CHANNELS), name='input')
+    logits, variables = model.model(x)
+    prediction = tf.nn.softmax(logits)
+
+    batch_data, batch_labels = tf.train.batch(
+        [test_data, test_labels],
+        batch_size=BATCH_SIZE,
+        capacity=5*BATCH_SIZE
+        )
+
+    if USE_GPU:
+        config = tf.ConfigProto()
+    else:
+        config = tf.ConfigProto(device_count={'GPU': 0})
+    saver = tf.train.Saver()
+
+    with tf.Session(config=config) as sess:
+
+        # Restore variables
+        saver.restore(sess=sess, save_path=checkpoint_file)
+        print("Restored variables from checkpoint %s" % checkpoint_file)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        predictions = []
+        labels = []
+
+        for step in range(sample_size // BATCH_SIZE):
+            _data, _labels = sess.run([batch_data, batch_labels])
+            _data = np.swapaxes(_data,0,1)
+            single_prediction = []
+            for i in range(8):
+                test_feed_dict = {x: _data[i]}
+                test_predictions = sess.run(prediction, feed_dict=test_feed_dict)
+                single_prediction.append(test_predictions)
+
+            # single_prediction is 8 * BATCH_SIZE * NUM_CAT
+            single_prediction = np.array(single_prediction)
+            single_prediction = np.swapaxes(single_prediction, 0, 1)
+
+            #single prediction is now BATCH_SIZE * 8 * NUM_CAT
+
+            # _labels is BATCH_SIZE
+            labels.extend(_labels)
+
+            predictions.extend(single_prediction.tolist()) 
+            # NUM_IMAGES * 8 * NUM_CAT
+            print('Processing batch number: %d / %d' % (step, sample_size//BATCH_SIZE))
+
+        # Save predictions in pickle file
+        filename = checkpoint_file.split('/')
+        if filename[-1] == '':
+            prefix = filename[-2]
+        else:
+            prefix = filename[-1]
+        prediction_file = PREDICTIONS_DIR + 'val_prediction__' + prefix
+        label_file = PREDICTIONS_DIR + 'val_labels__' + prefix
+        
+        with open(prediction_file, 'wb') as f:
+            pickle.dump(predictions, f)
+        with open(label_file, 'wb') as f:
+            pickle.dump(labels, f)
+
+        print('Created new pickle file with predictions in %s' % prediction_file)
+        
+        coord.request_stop()
+        coord.join(threads)
+
+    return (prediction_file, label_file)
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", type=str, help="Model Checkpoint File")
     parser.add_argument("-m", "--model", type=str , help="Model Type (AlexNet, VGGNet)")
     parser.add_argument("-g", "--aggregate", type=str, help="Aggregation method (average, product, max)")
+    parser.add_argument("-v", "--validation", action='store_true')
+    parser.set_defaults(validation=False)
+
     args = parser.parse_args()
 
     # run_test(args.file, args.model)
-    prediction_file = run_test(args.file, args.model)
-
-    make_submission_file(prediction_file, args.aggregate)
+    if args.validation:
+        prediction_file, label_file = run_validation(args.file, args.model)
+        evaluate_predictions(prediction_file,label_file)
+    else:
+        prediction_file = run_test(args.file, args.model)
+        make_submission_file(prediction_file, args.aggregate)
